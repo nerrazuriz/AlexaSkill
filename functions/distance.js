8bfe14ecfd23bce34a79156930c1047e6f17e25b
nerrazuriz <nerrazuriz@uc.cl>
2022-11-02 16:44:12 -0300

    feat-findNearestLocation

--- functions/distance.js
+++ functions/distance.js
@@ -1,11 +1,21 @@
+function distance(lat1,
+  lon1, lat2, lon2) {
+  lon1 = lon1 * Math.PI / 180;
+  lon2 = lon2 * Math.PI / 180;
+  lat1 = lat1 * Math.PI / 180;
+  lat2 = lat2 * Math.PI / 180;
 
-//function returns distance between two point
-function distance(x1, y1, x2, y2) {
-  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  // Formula de Haversine
+  let dlon = lon2 - lon1;
+  let dlat = lat2 - lat1;
+  let a = Math.pow(Math.sin(dlat / 2), 2)
+    + Math.cos(lat1) * Math.cos(lat2)
+    * Math.pow(Math.sin(dlon / 2), 2);
+
+  let c = 2 * Math.asin(Math.sqrt(a));
+
+  // Radio de la tierra en kilometros
+  let radio = 6371;
+
+  return (c * radio);
 }
-
-//function returns distance between two point in earth coordinates
-function distanceEarth(x1, y1, x2, y2) {
-  return distance(x1, y1, x2, y2) * 111.2;
-}
-
