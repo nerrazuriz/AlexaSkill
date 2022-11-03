f66ebbd9cc0247914d093fec59b65477b17a7d96
milosoria <camilo.soria@uc.cl>
2022-11-03 17:57:09 -0300

    fix

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const ngrok = require("ngrok");
 const app = express();
 const port = process.env.PORT || 3000;
 const alexaRoutes = require("./routes/alexaRoutes");
+const isDevelopment = process.env.NODE_ENV === "development";
 
 app.use("/alexa", alexaRoutes);
 app.listen(port, async () => {
-  const url = await ngrok.connect(port);
-  console.log(url);
+    if (isDevelopment) {
+      const url = await ngrok.connect(port);
+      console.log(url);
+    } else {
+        console.log(`Listening on port ${port}`);
+    }
 });
