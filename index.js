e36fbb986ef4f08b7062ffc97d6a06f82d933122
milosoria <camilo.soria@uc.cl>
2022-11-03 17:59:07 -0300

    fix

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const ngrok = require("ngrok");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,11 +7,15 @@ const alexaRoutes = require("./routes/alexaRoutes");
 const isDevelopment = process.env.NODE_ENV === "development";
 
 app.use("/alexa", alexaRoutes);
-app.listen(port, async () => {
-    if (isDevelopment) {
-      const url = await ngrok.connect(port);
-      console.log(url);
-    } else {
-        console.log(`Listening on port ${port}`);
-    }
-});
+
+if (isDevelopment) {
+  const ngrok = require("ngrok");
+  app.listen(port, async () => {
+    const url = await ngrok.connect(port);
+    console.log(url);
+  });
+} else {
+  app.listen(port, async () => {
+    console.log(`Listening on port ${port}`);
+  });
+}
